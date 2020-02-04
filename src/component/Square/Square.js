da18b07a9bda277a844db0c6d6ac2e025d564625
Batura Andrew <baturaandrew@gmainl.com>
2020-02-04 19:48:43 +0300

    feat: get winner

--- src/component/Square/Square.js
+++ src/component/Square/Square.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import './square.scss';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  handleClick() {
-    this.setState({ value: 'X' });
-  }
-
-  render() {
-    return (
-      <button className="square" onClick={(e) => this.handleClick(e)}>
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 export default Square;
